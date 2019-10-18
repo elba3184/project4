8b4277444a9ba482685627775b6e6812d27fa719
Elba Chimilio <elbachimilio@gmail.com>
2019-10-18 10:48:51 -0400

    sddsfgsdf

--- client/src/components/pages/Profile.jsx
+++ client/src/components/pages/Profile.jsx
@@ -73,25 +73,30 @@ class Profile extends Component {
   showCard = () => {
     return (
       <div className="profile-card">
-        <div className="profile-info">
+      <div className="profile-info">
 
 
-          <div class="col-md-4">
-            <div class="card profile-card-3">
-              <div class="background-block">
-                <img src={this.state.profilePhoto} alt="profile-sample1" class="background" />
-              </div>
-              <div class="profile-thumb-block">
-                <img src="https://randomuser.me/api/portraits/men/41.jpg" alt="profile-image" class="profile" />
-              </div>
-              <div class="card-content">
-                <h2>{this.state.first_name} {this.state.last_name}<small>{this.state.userType}</small></h2>
-              </div>
-              <p class="mt-3 w-100 float-left text-center"><strong>Modren Profile Card</strong></p>
+        <div class="col-md-4">
+          <div class="card profile-card-3">
+            <div class="background-block">
+              <img src="https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" alt="profile-sample1" class="background" />
             </div>
-            <p>{this.state.bio}</p>
+            <div class="profile-thumb-block">
+              <img src="https://randomuser.me/api/portraits/men/41.jpg" alt="profile-image" class="profile" />
+            </div>
+            <div class="card-content">
+              <h2>Justin Mccoy<small>Designer</small></h2>
+              <div class="icon-block"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"> <i class="fa fa-twitter"></i></a><a href="#"> <i class="fa fa-google-plus"></i></a></div>
+            </div>
           </div>
+          <p class="mt-3 w-100 float-left text-center"><strong>Modren Profile Card</strong></p>
         </div>
+        <img src={this.state.imageUrl} />
+        <h1>Name: {this.state.first_name} {this.state.last_name}</h1>
+        <h1>username: {this.state.username}</h1>
+        <p>{this.state.bio}</p>
+        <p>I'm a {this.state.userType}</p>
+      </div>
         {/* 
         <Modal
           closeModal={this.toggleModal}
